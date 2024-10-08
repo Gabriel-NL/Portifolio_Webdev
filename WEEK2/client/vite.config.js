35abbcb75afe6fab65dd611a181738fae068e943
Gabriel Nogueira Lages <78828285+Gabriel-NL@users.noreply.github.com>
2024-10-08 02:06:52 -0400

    Updating vite

--- WEEK2/client/vite.config.js
+++ WEEK2/client/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portifolio_Webdev/', // Add the base path for GitHub Pages
 })
